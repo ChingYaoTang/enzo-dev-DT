1a66ee8c153e073e8ab08c3603d656e0399301d9
ChingYaoTang <410414208@gms.ndhu.edu.tw>
2020-02-26 02:21:36 -0800

    Save the original version
    Copy from enzo-test/enzo-dev/ and remove "-g" flag in the make file.
    Create personal git repo at enzo-test/enzo-dev-DT/src/ and commit the original version.

---
+++ enzo/auto_show_version.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void auto_show_version(FILE *fp) {
+   fprintf (fp,"\n");
+   fprintf (fp,"Git Branch   master\n");
+   fprintf (fp,"Git Revision d9db053292fa478136387cb922cc85eb61b3857c\n");
+   fprintf (fp,"\n");
+}
