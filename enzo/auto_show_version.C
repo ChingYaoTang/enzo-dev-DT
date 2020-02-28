2458e633f30b60e92c083ce9d75220a2c4171450
ChingYaoTang <410414208@gms.ndhu.edu.tw>
2020-02-27 20:43:13 -0800

    Successfully compile the original version

---
+++ enzo/auto_show_version.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void auto_show_version(FILE *fp) {
+   fprintf (fp,"\n");
+   fprintf (fp,"Git Branch   master\n");
+   fprintf (fp,"Git Revision 3a9200ce44784bbfd0244aed8152692aa74ce244\n");
+   fprintf (fp,"\n");
+}
